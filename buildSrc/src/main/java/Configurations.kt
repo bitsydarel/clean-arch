cd9d4f857714fc1ced370860cd1d71089f7b290e
Darel Bitsy <bitsydarel@gmail.com>
2018-12-13 02:23:24 +0200

    Created buildSrc to organize build configuration

---
+++ buildSrc/src/main/java/Configurations.kt
@@ -0,0 +1,7 @@
+object Configurations {
+    const val kotlinLanguageVersion: String = "1.3.11"
+    const val jvmDefaultVersion = "1.8"
+    const val groupId: String = "com.dbeginc.clean-arch"
+    const val cleanArchBaseVersion: String = "1.0-SNAPSHOT"
+    const val cleanArchRxJavaVersion: String = "1.0-SNAPSHOT"
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,8 @@
+object KotlinLibraries {
+    const val stdlibJdk8: String = "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
+    const val stdlibJdk7: String = "org.jetbrains.kotlin:kotlin-stdlib-jdk7"
+}
+
+object ThirdPartiesLibraries {
+    const val rxJava2: String = "io.reactivex.rxjava2:rxjava:2.2.4"
+}
